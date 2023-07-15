c9af3d6c9ff79bf6b09c83b1b026da8022322101
ballban_Jung <ballban@hotmail.com>
2023-07-16 03:13:52 +0900

    challenge31

--- challenge30.py
+++ challenge30.py
@@ -5,6 +5,6 @@ def MD_padding(message: bytes, extra_len = 0):
     origin_message_len = len(message)
     message += b'\x80'
     message += b'\x00' * (-(len(message) + 8) % 64)
-    message += struct.pack(b'>Q', (origin_message_len + extra_len) * 8)
+    message += struct.pack(b'<Q', (origin_message_len + extra_len) * 8)
 
     return message
