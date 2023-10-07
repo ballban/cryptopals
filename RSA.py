5108e75341cf0e7d6ef714cf62917b6629703b8d
ballban_Jung <ballban@hotmail.com>
2023-10-08 01:48:28 +0900

    Challenge 39

---
+++ RSA.py
@@ -0,0 +1,38 @@
+from Crypto.Util import number
+
+class RSA:
+    def __init__(self) -> None:
+        self.e = 3
+        self.p = self.q = self.et = 0
+
+        while self.et % self.e == 0:
+            size = 2048
+            self.p = number.getPrime(size)
+            self.q = number.getPrime(size)
+            self.et = (self.p - 1) * (self.q - 1)
+
+        self.N = self.p * self.q
+
+        self.d = pow(self.e, -1, self.et)
+
+    
+    def EGCD(self, a:int, b:int) -> tuple:
+        old_r, r = a, b
+        old_s, s = 1, 0
+        old_t, t = 0, 1
+        while r > 0:
+            q = old_r // r
+            old_r, r = r, old_r - q * r
+            old_s, s = s, old_s - q * s
+            old_t, t = t, old_t - q * t
+        print(r, s, t)
+        return old_r, old_s, old_t
+    
+    def encrypt(self, text:str) -> int:
+        m = int(text.encode().hex(), 16)
+        c = pow(m, self.e, self.N)
+        return c
+
+    def decript(self, c:int) -> str:
+        m = pow(c, self.d, self.N)
+        return bytes.fromhex(hex(m)[2:]).decode()
